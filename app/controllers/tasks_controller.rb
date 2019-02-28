353dd42e1063310c45217e9b30c10a4c6470df42
naohiro.ookubo <keyanao_0620@yahoo.co.jp>
2019-02-28 13:21:36 +0000

    bootstrap

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit,:update, :destroy]
   def index
       @tasks = Task.all
+      @tasks = Task.page(params[:page]).per(10).order(:id)
   end
 
   def show
